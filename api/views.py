8dda4f01237af83cdb6d9ba8cb66594a14e83325
ritik <ritikbhatt020@gmail.com>
2024-02-12 22:44:30 +0530

    🏹serializers and task-list done

--- api/views.py
+++ api/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import TaskSerializer
+from .models import Task
+
 # Create your views here.
 
+@api_view(['GET'])
 def apiOverview(request):
-    return JsonResponse("API BASE POINT", safe=False)
+    api_urls = {
+		'List':'/task-list/',
+		'Detail View':'/task-detail/<str:pk>/',
+		'Create':'/task-create/',
+		'Update':'/task-update/<str:pk>/',
+		'Delete':'/task-delete/<str:pk>/',
+		}
+    return Response(api_urls)
+
+# Serializers allow us to return any model object here, a json response.. its just gonna be a json obj
+
+@api_view(['GET'])
+def taskList(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
+
